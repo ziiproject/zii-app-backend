6487573be0908c8afbe8e5e895f1aa8077c11735
ziiproject <ziiproject1@gmail.com>
2025-05-08 11:05:34 -0400

    Fix PostgreSQL SSL connection

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,11 @@
 import pkg from 'pg';
-const { Pool } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 
+const { Pool } = pkg;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false  
-  }
+  connectionString: process.env.DATABASE_URL 
 });
 
 export default pool;
